6320036314f92cd683067c4d6ce7189400244bae
Sacag08 <sachitagarwal98@gmail.com>
2024-10-11 22:07:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "wasteDetection",
+    version= "0.0.0",
+    author= "Sachit Agarwal",
+    author_email="sachitagarwal98@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
